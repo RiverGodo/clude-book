bb1ab11c474c88018d10d8ea6064fdd28987e7b5
WangShaoqi <924384288@qq.com>
2018-09-06 22:00:10 +0800

    day4

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,6 @@
 //index.js
 //获取应用实例
-import {fetch} from '../../utils/util.js'
+import {fetch,login} from '../../utils/util.js'
 const app = getApp();
 
 Page({
@@ -12,35 +12,69 @@ Page({
     autoplay: true,
     interval: 3000,
     duration: 1000,
-    isLoading: false
+    isLoading: false,
+    pn:1,
+    hasMore:true,
+    loadDone:false
   },
   onLoad (){
-    this.getData(),
-    this.getContent()
+    login()
+    Promise.all([this.getData(), this.getContent()]).then(() => {
+      this.setData({
+        hasMore: true,
+        pn: 1,
+        loadDone:true
+      })
+ 
+    })
   
   },
-  getData(){
+  getData(){//获取轮播图
+  return new Promise((resolve,reject)=>{
+
     this.setData({
       isLoading: true
     })
-    fetch.get('/swiper').then(res =>{
-     this.setData({
-       swiperData : res.data,
-       isLoading: false
-     })
-    }).catch(err=>{
+    fetch.get('/swiper').then(res => {
+      resolve()
+      this.setData({
+        swiperData: res.data,
+        isLoading: false
+      })
+    }).catch(err => {
+      reject()
       this.setData({
         isLoading: false
       })
     })
+  })
+    
   },
-  getContent(){
-    fetch.get('/category/books').then(res=>{
-      console.log(res)
+  getContent(){//获取书籍列表
+  return new Promise((resolve,reject)=>{
+    this.setData({
+      isLoading: true
+    })
+    fetch.get('/category/books').then(res => {
+      resolve()
       this.setData({
-        mainContent: res.data
+        mainContent: res.data,
+        isLoading:false
       })
     })
+  })
+  },
+  getMoreContent(){
+    return new Promise(resolve=>{
+      fetch.get('/category/books', { pn: this.data.pn }).then(res => {
+        let newArr = [...this.data.mainContent,...res.data]
+        this.setData({
+          mainContent: newArr,
+        })
+        resolve(res)
+      })
+    })
+    
   },
   jumpBook(event){
     const id = event.currentTarget.dataset.id
@@ -51,5 +85,30 @@ Page({
   
   },
 
+  onPullDownRefresh(){
+    Promise.all([this.getData(), this.getContent()]).then(()=>{
+      this.setData({
+        hasMore:true,
+        pn:1
+      })
+      wx.stopPullDownRefresh();
+    })
+  },
+  onReachBottom(){
+    if (this.data.hasMore){
+      this.setData({
+        pn: this.data.pn + 1
+      })
+      this.getMoreContent().then(res => {
+        if (res.data.length < 2) {
+          this.setData({
+            hasMore: false
+          })
+
+        }
+      })
+    }
+ 
+  },
   
 })
--- utils/util.js
+++ utils/util.js
@@ -3,14 +3,27 @@ const baseUrl = "https://m.yaojunrong.com"
 const fetch = {
   http(url, method, data){
     return new Promise((resolve,reject)=>{
+     let token = wx.setStorageSync('token')
+
+      let header ={
+        
+        'content-type': "application/json"
+      
+      }
+      if(token){
+        header.token = token
+      }
+
       wx.request({
         url: baseUrl + url,
         data,
         method,
-        header: {
-          'content-type': "application/json"
-        },
+        header,
         success(res) {
+          console.log(res)
+          if(res.header.Token){
+            wx.setStorageSync('token', res.header.Token)
+          }
           resolve(res.data)
         },
         fail(err) {
@@ -21,8 +34,28 @@ const fetch = {
   },
   get(url, data){
    return this.http(url, 'GET',data)
+  },
+  post(url,data){
+    return this.http(url,'POST',data)
   }
 
 }
 
+const login = ()=>{
+  wx.login({
+    success(res){
+
+      fetch.post('/login',{
+        code:res.code,
+        appid:"wx24aaa4dd5c1f62a6",
+        secret:"1e95032030065ed401d6ae702e5c1c33"
+      }).then(res=>{
+        // console.log(res)
+      })
+
+    }
+  })
+}
+
 exports.fetch = fetch;
+exports.login = login;
